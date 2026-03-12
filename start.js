db8f24aeb436ecd68ce76933234ac022a838b951
drake9696 <my1junk2mail3@gmail.com>
2026-03-12 02:00:09 -0400

    Update start.js

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        message: "uv run uvicorn speaches.main:app --host 0.0.0.0 --port 8000",
+        message: "uv run python -m speaches",
         path: "app",
         env: {
           SPEACHES__HOST: "0.0.0.0",
